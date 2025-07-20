5961e35d4017fa7d04653b1b9e5c878f26f6dfa3
Oscar Kosar-Kosarewicz <okosarkosarewicz@gmail.com>
2025-07-20 16:28:37 -0400

    Add support for HealthDisplay

---
+++ Source/UltrawideUIHealthDisplayPatch/HealthDisplayPatch.cs
@@ -0,0 +1,58 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using Verse;
+
+namespace UltrawideUI
+{
+    [StaticConstructorOnStartup]
+    public class HealthDisplayPatch
+    {
+
+        static Type targetType = AccessTools.TypeByName("HarmonyPatches");
+        static MethodInfo targetMethod = AccessTools.Method(targetType, "HealthDisplayWindow_UpdateWindowSize");
+
+        static HealthDisplayPatch()
+        {
+            var harmony = new Harmony("UltrawideUI.HealthDisplayPatch");
+            var assembly = Assembly.GetExecutingAssembly();
+            harmony.PatchAll(assembly);
+        }
+        static Type TargetMethod()
+        {
+            return AccessTools.TypeByName("HarmonyPatches");
+        }
+
+        [HarmonyPatch]
+        public static class HealthDisplay_HarmonyPatches_HealthDisplayWindow_UpdateWindowSize_Patch
+        {
+            static MethodInfo TargetMethod()
+            {
+                var targetType = AccessTools.TypeByName("GTHealthDisplay.HealthDisplayWindow");
+                return targetType.GetMethod("UpdateWindowSize", BindingFlags.NonPublic | BindingFlags.Static);
+            }
+
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                for (var i = 0; i < codes.Count; i++)
+                {
+                    if (codes[i].opcode == OpCodes.Ldc_R4 && (float)codes[i].operand == 0f)
+                    {
+                        codes.Insert(i + 1, new CodeInstruction(OpCodes.Ldsfld, UltrawideUI.ScreenWidthFieldInfo));
+                        codes.Insert(i + 2, new CodeInstruction(OpCodes.Conv_R4));
+                        codes.Insert(i + 3, new CodeInstruction(OpCodes.Ldsfld, UltrawideUI.LeftMultiplierFieldInfo));
+                        codes.Insert(i + 4, new CodeInstruction(OpCodes.Conv_R4));
+                        codes.Insert(i + 5, new CodeInstruction(OpCodes.Mul));
+                        codes.RemoveAt(i);
+                        break;
+                    }
+                }
+                return codes.AsEnumerable();
+            }
+        }
+    }
+}
