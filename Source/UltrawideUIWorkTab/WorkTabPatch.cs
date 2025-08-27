90547f653aa5cead09ce35cabcf22935adcc7187
Oscar Kosar-Kosarewicz <okosarkosarewicz@gmail.com>
2025-08-26 22:39:04 -0400

    Add support for worktab

---
+++ Source/UltrawideUIWorkTab/WorkTabPatch.cs
@@ -0,0 +1,136 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using WorkTab;
+using Verse;
+
+namespace UltrawideUI
+{
+    [StaticConstructorOnStartup]
+    public class WorkTabPatch
+    {
+        static WorkTabPatch()
+        {
+            var harmony = new Harmony("UltrawideUI.WorkTabPatch");
+            var assembly = Assembly.GetExecutingAssembly();
+            harmony.PatchAll(assembly);
+        }
+
+        [HarmonyPatch(typeof(WorkTab.PawnTable_PawnTableOnGUI), "InitialSize")]
+        public static class WorkTab_PawnTable_PawnTableOnGUI_InitialSize
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                for (var i = 0; i < codes.Count; i++)
+                {
+                    if (codes[i].opcode == OpCodes.Ldsfld && codes[i].operand is FieldInfo fieldInfo)
+                    {
+                        if (fieldInfo.Name == "screenWidth" && fieldInfo.DeclaringType.FullName == "Verse.UI")
+                        {
+                            codes.Insert(i + 2, new CodeInstruction(OpCodes.Ldsfld, UltrawideUI.UIWidthFieldInfo));
+                            codes.Insert(i + 3, new CodeInstruction(OpCodes.Conv_R4));
+                            codes.Insert(i + 4, new CodeInstruction(OpCodes.Mul));
+                        }
+                    }
+                }
+                return codes.AsEnumerable();
+            }
+        }
+
+        [HarmonyPatch(typeof(WorkTab.MainTabWindow_WorkTab), "DoWindowContents")]
+        public static class WorkTab_MainTabWindow_WorkTab_DoWindowContents
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                for (var i = 0; i < codes.Count; i++)
+                {
+                    if (codes[i].opcode == OpCodes.Ldsfld && codes[i].operand is FieldInfo fieldInfo)
+                    {
+                        if (fieldInfo.Name == "screenWidth" && fieldInfo.DeclaringType.FullName == "Verse.UI")
+                        {
+                            codes.Insert(i + 2, new CodeInstruction(OpCodes.Ldsfld, UltrawideUI.UIWidthFieldInfo));
+                            codes.Insert(i + 3, new CodeInstruction(OpCodes.Conv_R4));
+                            codes.Insert(i + 4, new CodeInstruction(OpCodes.Mul));
+                            break;
+                        }
+                    }
+                }
+                return codes.AsEnumerable();
+            }
+        }
+
+        [HarmonyPatch(typeof(WorkTab.MainTabWindow_WorkTab), "RecacheTimeBarRect")]
+        public static class WorkTab_MainTabWindow_WorkTab_RecacheTimeBarRect
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                for (var i = 0; i < codes.Count; i++)
+                {
+                    if (codes[i].opcode == OpCodes.Ldsfld && codes[i].operand is FieldInfo fieldInfo)
+                    {
+                        if (fieldInfo.Name == "screenWidth" && fieldInfo.DeclaringType.FullName == "Verse.UI")
+                        {
+                            codes.Insert(i + 2, new CodeInstruction(OpCodes.Ldsfld, UltrawideUI.UIWidthFieldInfo));
+                            codes.Insert(i + 3, new CodeInstruction(OpCodes.Conv_R4));
+                            codes.Insert(i + 4, new CodeInstruction(OpCodes.Mul));
+                            break;
+                        }
+                    }
+                }
+                return codes.AsEnumerable();
+            }
+        }
+
+        [HarmonyPatch(typeof(WorkTab.MainTabWindow_PawnTable_SetDirty), "Prefix")]
+        public static class WorkTab_MainTabWindow_PawnTable_SetDirty_Prefix
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                for (var i = 0; i < codes.Count; i++)
+                {
+                    if (codes[i].opcode == OpCodes.Ldsfld && codes[i].operand is FieldInfo fieldInfo)
+                    {
+                        if (fieldInfo.Name == "screenWidth" && fieldInfo.DeclaringType.FullName == "Verse.UI")
+                        {
+                            codes.Insert(i + 2, new CodeInstruction(OpCodes.Ldsfld, UltrawideUI.UIWidthFieldInfo));
+                            codes.Insert(i + 3, new CodeInstruction(OpCodes.Conv_R4));
+                            codes.Insert(i + 4, new CodeInstruction(OpCodes.Mul));
+                            break;
+                        }
+                    }
+                }
+                return codes.AsEnumerable();
+            }
+        }
+
+        [HarmonyPatch(typeof(WorkTab.MainTabWindow_PawnTable_DoWindowContents), "Postfix")]
+        public static class WorkTab_MainTabWindow_PawnTable_DoWindowContents_Postfix
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                for (var i = 0; i < codes.Count; i++)
+                {
+                    if (codes[i].opcode == OpCodes.Ldsfld && codes[i].operand is FieldInfo fieldInfo)
+                    {
+                        if (fieldInfo.Name == "screenWidth" && fieldInfo.DeclaringType.FullName == "Verse.UI")
+                        {
+                            codes.Insert(i + 2, new CodeInstruction(OpCodes.Ldsfld, UltrawideUI.UIWidthFieldInfo));
+                            codes.Insert(i + 3, new CodeInstruction(OpCodes.Conv_R4));
+                            codes.Insert(i + 4, new CodeInstruction(OpCodes.Mul));
+                            break;
+                        }
+                    }
+                }
+                return codes.AsEnumerable();
+            }
+        }
+    }
+}
